b519ffccb9b735dedbf63f85fe59c1c1c30671e3
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-22 02:22:27 -0300

    feat: change site address

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_URL = 'https://assine-clarobrasil.com';
+export const SITE_URL = 'https://assine-clarobrasil-com-br.vercel.app';
 export const SITE_NAME = "Assine Claro Brasil";
 export const SITE_TITLE = "Assine Claro Brasil - Com Fibra, 5G e muito mais!";
 export const SITE_DESCRIPTION = "A Claro oferece combos de Internet fixa com fibra, Internet Móvel, TV por Assinatura, Planos de celular com o 4G e 5G, Apps e muito mais. Confira!";
